ccea5a4c379cf7576efc44325cfd6419133eaea6
WinsonWu <waifeng_ng@hotmail.com>
2023-10-29 03:41:20 +0800

    Adding giftbox

--- game/application.0d3df.js
+++ game/application.0d3df.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.fa340.json';
+          this.settingsPath = 'src/settings.3fc4f.json';
           this.showFPS = false;
         }
 
--- game/index.17340.js
+++ game/index.17340.js
@@ -1,4 +1,4 @@
-System.register(["./application.8afeb.js"], function (_export, _context) {
+System.register(["./application.0d3df.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.8afeb.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application8afebJs) {
-      Application = _application8afebJs.Application;
+    setters: [function (_application0d3dfJs) {
+      Application = _application0d3dfJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
